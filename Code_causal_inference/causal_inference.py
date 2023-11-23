f0364a4a51852e929abda32271e4223062b80d8e
nqttruong <truongnqfx@gmail.com>
2023-11-23 21:10:32 +0700

    update

---
+++ Code_causal_inference/causal_inference.py
@@ -0,0 +1,27 @@
+from dowhy import CausalModel
+import dowhy.datasets
+
+# Generate some sample data
+data = dowhy.datasets.linear_dataset(
+    beta=10,
+    num_common_causes=5,
+    num_instruments=2,
+    num_samples=10000)
+
+# Step 1: Create a causal model from the data and given graph.
+model = CausalModel(
+    data=data["df"],
+    treatment=data["treatment_name"],
+    outcome=data["outcome_name"],
+    graph=data["gml_graph"])
+
+# Step 2: Identify causal effect and return target estimands
+identified_estimand = model.identify_effect()
+
+# Step 3: Estimate the target estimand using a statistical method.
+estimate = model.estimate_effect(identified_estimand,
+                                 method_name="backdoor.propensity_score_matching")
+
+# Step 4: Refute the obtained estimate using multiple robustness checks.
+refute_results = model.refute_estimate(identified_estimand, estimate,
+                                       method_name="random_common_cause")
